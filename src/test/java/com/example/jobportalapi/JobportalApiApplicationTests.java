d1d44b8b4322f52a9443a1abf590365a88780185
alievaleila <meliyeva452@gmail.com>
2026-04-25 19:53:37 +0400

    feat: initial project setup with Spring Boot and required dependencies

---
+++ src/test/java/com/example/jobportalapi/JobportalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobportalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobportalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
